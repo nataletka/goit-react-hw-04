1573fffe9ff00b3f8a2dea6769e485b8e0aeafe7
Nataliia Khinchuk <evettakh@gmail.com>
2025-05-14 02:06:06 +0300

    create components folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css';
 import { Toaster } from 'react-hot-toast';
 import { useEffect, useState } from 'react';
-import SearchBar from './SearchBar/SearchBar';
-import ImageGallery from './ImageGallery/ImageGallery';
-import Loader from './Loader/Loader';
-import ErrorMessage from './ErrorMessage/ErrorMessage';
-import LoadMoreBtn from './LoadMoreBtn/LoadMoreBtn';
-import ImageModal from './ImageModal/ImageModal';
+import SearchBar from './components/SearchBar/SearchBar';
+import ImageGallery from './components/ImageGallery/ImageGallery';
+import Loader from './components/Loader/Loader';
+import ErrorMessage from './components/ErrorMessage/ErrorMessage';
+import LoadMoreBtn from './components/LoadMoreBtn/LoadMoreBtn';
+import ImageModal from './components/ImageModal/ImageModal';
 import axios from 'axios';
 
 const API_KEY = '7K0FErjmdpsJXZohd2nnOhEV2zLNMWoA9dooMAjmTpc';
